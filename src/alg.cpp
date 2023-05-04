a08248c86c262c027a9d7238f484bca41b76d1f1
Leonildo01 <131868910+Leonildo01@users.noreply.github.com>
2023-05-04 19:54:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,27 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    
+ using namespace std;
+int NOD (int a, int b)
+{
+    if (b == 0)
+        return a;
+    if (a > b)
+        return NOD (b, a % b);
+    else
+        return NOD (a, b % a);
+}
+ 
+int main ()
+{
+    setlocale (0, "");
+    int a, b;
+    cout << "Enter number 2: " << endl;
+    cin >> a >> b;
+    cout << "The greatest common divisor is: " << NOD (b, a % b) << endl;
+    cin.get();
+    cin.get();
+    return 0;
+}
 }
